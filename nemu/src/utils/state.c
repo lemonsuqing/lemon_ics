2c68331c45618ae27a40a245962248066d5041e3
tracer-ics2023 <tracer@njuics.org>
2024-06-03 20:35:56 +0800

    >  compile NEMU 11451400 lemon Linux lemonsuqing 6.5.0-35-generic #35~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue May  7 09:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:35:56 up  5:38,  2 users,  load average: 0.57, 0.48, 0.47

--- nemu/src/utils/state.c
+++ nemu/src/utils/state.c
@@ -14,7 +14,7 @@
 ***************************************************************************************/
 
 #include <utils.h>
-// NEMUState nemu_state = { .state = NEMU_STOP };
+
 NEMUState nemu_state = { .state = NEMU_STOP };
 
 int is_exit_status_bad() {
