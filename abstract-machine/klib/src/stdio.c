414170411c2956291d5b83f5135f4acf33574609
tracer-ics2023 <tracer@njuics.org>
2024-06-05 17:42:56 +0800

    >  compile NEMU 11451400 lemon Linux lemonsuqing 6.5.0-35-generic #35~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue May  7 09:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:42:56 up 12:45,  1 user,  load average: 0.29, 0.22, 0.19

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -5,16 +5,107 @@
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
-int printf(const char *fmt, ...) {
-  panic("Not implemented");
+static void reverse(char *s, int len) {
+  char *end = s + len - 1;
+  char tmp;
+  while (s < end) {
+    tmp = *s;
+    *s = *end;
+    *end = tmp;
+  }
 }
 
-int vsprintf(char *out, const char *fmt, va_list ap) {
-  panic("Not implemented");
+static int itoa(int n, char *s, int base) {
+  assert(base <= 16);
+
+  int i = 0, sign = n, bit;
+  if (sign < 0) n = -n;
+  do {
+    bit = n % base;
+    if (bit >= 10) s[i++] = 'a' + bit - 10;
+    else s[i++] = '0' + bit;
+  } while ((n /= base) > 0);
+  if (sign < 0) s[i++] = '-';
+  s[i] = '\0';
+  reverse(s, i);
+
+  return i;
 }
 
 int sprintf(char *out, const char *fmt, ...) {
-  panic("Not implemented");
+  va_list pArgs;
+  va_start(pArgs, fmt);
+  char *start = out;
+  
+  for (; *fmt != '\0'; ++fmt) {
+    if (*fmt != '%') {
+      *out = *fmt;
+      ++out;
+    } else {
+      switch (*(++fmt)) {
+      case '%': *out = *fmt; ++out; break;
+      case 'd': out += itoa(va_arg(pArgs, int), out, 10); break;
+      case 's':
+        char *s = va_arg(pArgs, char*);
+        strcpy(out, s);
+        out += strlen(out);
+        break;
+      }
+    }
+  }
+  *out = '\0';
+  va_end(pArgs);
+
+  return out - start;
+}
+
+int vsprintf(char *out, const char *fmt, va_list ap) {
+  
+  char *start = out;
+    
+  for(;*fmt != '\0';++fmt){
+    if(*fmt != '%'){
+      *out = *fmt;
+      ++out;
+    }
+    else{
+     switch(*(++fmt)){
+     case '%': *out = *fmt; ++out; break;
+     case 'd': out += itoa(va_arg(ap,int),out,10);break;
+     case 's':
+        {
+        char *s = va_arg(ap,char*);
+        strcpy(out,s);
+        out += strlen(out);
+        }
+        break;
+     case 'c':
+       {
+          char c = (char)va_arg(ap,int);
+          *out++ = c;
+       }
+       break;
+     }
+     
+    }
+   }
+    *out = '\0';
+
+    return out - start;
+ // panic("Not implemented");
+}
+
+int printf(const char *fmt, ...) {
+  char buf[1024];
+	va_list ap;
+	va_start(ap, fmt);
+
+	int res = vsprintf(buf, fmt, ap);
+	putstr(buf);
+
+	va_end(ap);
+	return res;
+  //panic("Not implemented");
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
