e61282933433942f472a52168e03f2b9f25ca83c
tracer-ics2023 <tracer@njuics.org>
2024-06-05 11:22:37 +0800

    >  compile NEMU 11451400 lemon Linux lemonsuqing 6.5.0-35-generic #35~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue May  7 09:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:22:37 up 11:20,  1 user,  load average: 0.14, 0.17, 0.17

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -25,7 +25,14 @@ char *strcpy(char *dst, const char *src) {
 }
 
 char *strncpy(char *dst, const char *src, size_t n) {
-  panic("Not implemented");
+  char *ret = dst;
+  while(n && (*dst++ = *src++)) {
+    n--;
+  }
+  while(n--) {
+    *dst++ = '\0';
+  }
+  return ret;
 }
 
 char *strcat(char *dst, const char *src) {
@@ -49,7 +56,14 @@ int strcmp(const char *s1, const char *s2) {
 }
 
 int strncmp(const char *s1, const char *s2, size_t n) {
-  panic("Not implemented");
+  for(; n > 0; s1++, s2++, --n) {
+    if(*s1 != *s2) {
+      return ((unsigned char)*s1 - (unsigned char)*s2);
+    } else if(*s1 == '\0') {
+      return 0;
+    }
+  }
+  return 0;
 }
 
 void *memset(void *s, int c, size_t n) {
@@ -60,9 +74,21 @@ void *memset(void *s, int c, size_t n) {
     return s;
 }
 
-
 void *memmove(void *dst, const void *src, size_t n) {
-  panic("Not implemented");
+  char *d = dst;
+  const char *s = src;
+  if (d<s) {
+    while (n--) {
+      *d++ = *s++;
+    }
+  } else {
+    d = d + n - 1;
+    s = s + n - 1;
+    while (n--) {
+      *d-- = *s--;
+    }
+  }
+  return dst;
 }
 
 void *memcpy(void *out, const void *in, size_t n) {
@@ -75,7 +101,16 @@ void *memcpy(void *out, const void *in, size_t n) {
 }
 
 int memcmp(const void *s1, const void *s2, size_t n) {
-  panic("Not implemented");
+  const unsigned char *p1 = s1, *p2 = s2;
+  while(n--) {
+    if( *p1 != *p2 ) {
+      return *p1 - *p2;
+    } else {
+      p1++;
+      p2++;
+    }
+  }
+  return 0;
 }
 
 #endif
