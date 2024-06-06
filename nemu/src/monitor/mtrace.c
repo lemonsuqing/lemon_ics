9bc1ad5ac27a9d3ab21cb9565425e7af74b306c1
tracer-ics2023 <tracer@njuics.org>
2024-06-06 14:51:20 +0800

    >  compile NEMU 11451400 lemon Linux lemonsuqing 6.5.0-35-generic #35~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue May  7 09:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:51:20 up  3:29,  1 user,  load average: 0.60, 0.38, 0.29

--- nemu/src/monitor/mtrace.c
+++ nemu/src/monitor/mtrace.c
@@ -1,11 +1,9 @@
 #include <trace.h>
 
 void mtrace_print(vaddr_t addr, int len, word_t data,char op){
-    printf("%c\n",op);
     if(op == 'r'){
-        printf("123\n");
-        printf("read 0x%08x %d byte. Data: 0x%08x\n", addr, len, data);
+        printf(ANSI_FMT("read 0x%08x %d byte. Data: 0x%08x", ANSI_FG_YELLOW), addr, len, data);
     }else{
-        log_write(ANSI_FMT("write %d byte data into 0x%08x. data: 0x%08x", ANSI_BG_WHITE)"\n",len, addr, data);
+        printf(ANSI_FMT("write %d byte data into 0x%08x. data: 0x%08x", ANSI_BG_WHITE)"\n",len, addr, data);
     }
 }
