04872d451f16afe56066d165c8d6b96c4c19ba01
tracer-ics2023 <tracer@njuics.org>
2024-06-06 10:58:08 +0800

    >  compile NEMU 11451400 lemon Linux lemonsuqing 6.5.0-35-generic #35~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue May  7 09:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:58:08 up  2:35,  1 user,  load average: 0.16, 0.16, 0.18

---
+++ nemu/include/trace.h
@@ -0,0 +1,11 @@
+#ifndef __TRACE__
+#define __TRACE__
+
+#include "./common.h"
+
+#define word_t uint32_t
+// void push_to_iringbuf(const char *logbuf,paddr_t pc);
+// void print_iringbuf(void);
+void mtrace(paddr_t addr, int len, word_t data,char op);
+#undef word_t
+#endif
