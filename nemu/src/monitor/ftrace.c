4d53fb58a9bc538e55a5fd1c51cdfe336736e97e
tracer-ics2023 <tracer@njuics.org>
2024-06-08 22:08:32 +0800

    >  compile NEMU 11451400 lemon Linux lemonsuqing 6.5.0-35-generic #35~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue May  7 09:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:08:32 up 11 min,  1 user,  load average: 0.27, 0.33, 0.26

--- nemu/src/monitor/ftrace.c
+++ nemu/src/monitor/ftrace.c
@@ -1,87 +1,199 @@
-#include <common.h>
 #include <elf.h>
-#include <stdlib.h>
+#include <common.h>
+#include <fcntl.h>
+#include <string.h>
+#include <unistd.h>
 
-#ifdef CONFIG_FTRACE
-uint32_t FUNC_NUM = 128;
-/* Store the ELF file header. */
-Elf32_Ehdr *Elf_Ehdr;
-/* Store the ELF section header. */
-Elf32_Shdr *Elf_Shdr, *Elf_Shdrs;
-/* Store the ELF symbol table entry. */
-Elf32_Sym *Elf_Sym, *Elf_Syms;
+#define MAX_DEPTH 64 // 栈的最大深度
 
-typedef struct {
-    uint32_t addr;
-    char func_name[16];
-    uint8_t type; 
-}FTRACE_FUNC;
-#define FUNC_NUM1 128
-FTRACE_FUNC ftrace_func[FUNC_NUM1];
+/*创建字符串记录表*/
+typedef struct{
+    char s_name[32];
+    paddr_t addr;
+    unsigned char info;
+    Elf32_Xword size;
+}Stm_Table;
 
+/*建立数组表单记录*/
+Stm_Table *S_table = NULL;
+int S_table_size = 0;
+int Fun_depth = 0;
 
-int i = 0, j = 0;
-char *string_table;
-char value_table[128];
+typedef struct fun_node{
+    paddr_t pc;
+    paddr_t depend;
+    struct fun_node *next;
+}FunNode;
+FunNode *fun_node_head = NULL;
 
-void parse_elf(const char *elf_file)
-{
-    FILE *file = fopen(elf_file, "r");
-    if (!file)
-    {
-        perror("fopen");
-        exit(1);
+typedef struct function{
+    char fun_name[32];
+    paddr_t fun_addr;
+}function;
+
+function fun_list[64];
+int fun_index;
+char *fun_name_stack[MAX_DEPTH]; // 函数名栈
+int fun_name_stack_top = 0; // 栈顶指针
+
+void push_fun_name(char *fun_name) {
+    if (fun_name_stack_top < MAX_DEPTH) {
+        fun_name_stack[fun_name_stack_top++] = fun_name;
+    } else {
+        printf("Function call depth exceeds maximum limit!\n");
     }
+}
 
-    /* Read the ELF header */
-    Elf_Ehdr = (Elf32_Ehdr *)malloc(sizeof(Elf32_Ehdr));
-    if(fread(Elf_Ehdr, sizeof(Elf32_Ehdr), 1, file));
-
-    /* Read the section headers */
-    Elf_Shdrs = (Elf32_Shdr *)malloc(sizeof(Elf32_Shdr) * Elf_Ehdr->e_shnum);
-    /* 
-     * use fseek() to move the file position indicator to the desired location before calling fread().
-     * then read data from a specific location within a file without changing the file position indicator
-     */
-    fseek(file, Elf_Ehdr->e_shoff, SEEK_SET);
-    /* To be figured: if use assert will cause core dump, why? */
-    if(fread(Elf_Shdrs, sizeof(Elf32_Shdr), Elf_Ehdr->e_shnum, file));
-
-    /* Find the symbol table section */
-    for (i = 0; i < Elf_Ehdr->e_shnum; i++)
-    {
-        Elf_Shdr = &Elf_Shdrs[i];
-        if (Elf_Shdr->sh_type == SHT_SYMTAB)
-        {
-            /* Read the symbol table */
-            Elf_Syms = Elf_Sym = (Elf32_Sym *)malloc(Elf_Shdr->sh_size);
-            fseek(file, Elf_Shdr->sh_offset, SEEK_SET);
-            if(fread(Elf_Sym, Elf_Shdr->sh_size, 1, file));
-
-            /* Find the associated string table */
-            string_table = (char *)malloc(Elf_Shdrs[Elf_Shdr->sh_link].sh_size);
-            fseek(file, Elf_Shdrs[Elf_Shdr->sh_link].sh_offset, SEEK_SET); /* symtab location */
-            if(fread(string_table, Elf_Shdrs[Elf_Shdr->sh_link].sh_size, 1, file));
-            FUNC_NUM = Elf_Shdr->sh_size / sizeof(Elf32_Sym);
-
-            /* Print the symbols */
-            for (j = 0; j < FUNC_NUM; j++)
-            {
-                sprintf(ftrace_func[j].func_name, "%s", string_table + Elf_Sym[j].st_name);
-                ftrace_func[j].type= Elf_Sym[j].st_info;
-                ftrace_func[j].addr = Elf_Sym[j].st_value;
-            }
-            for (j = 0; j < FUNC_NUM; j++)
-            {
-                log_write("0x%x\n", ftrace_func[j].addr);
-                log_write("%s\n", ftrace_func[j].func_name);
-            }
+// void push_fun_name(char *fun_name) {
+//     if (fun_name_stack_top < MAX_DEPTH) {
+//         fun_name_stack[fun_name_stack_top++] = fun_name;
+//         printf("Current function call stack:\n");
+//         ftrace_write("\n==> stack name :");
+//         for(int i = 0; i < fun_name_stack_top; i++) {
+//             ftrace_write("%s, ", fun_name_stack[i]);
+//         }
+//         ftrace_write("\n\n");
+//     } else {
+//         printf("Function call depth exceeds maximum limit!\n");
+//     }
+// }
 
 
-            free(Elf_Syms);
-            free(string_table);
+// 从栈中弹出函数名
+char* pop_fun_name() {
+    if (fun_name_stack_top > 0) {
+        return fun_name_stack[--fun_name_stack_top];
+    } else {
+        printf("Function call stack is empty!\n");
+        return NULL;
+    }
+}
+
+// 读取elf头判断是否为elf文件
+static void Read_ELFhead(int fd, Elf32_Ehdr *eh){
+    assert(lseek(fd, 0, SEEK_SET) == 0);
+    assert(read(fd, (void *)eh, sizeof(Elf32_Ehdr)) == sizeof(Elf32_Ehdr));
+    if(memcmp((char*)eh->e_ident, "\177ELF", 4)){
+        panic("no a ELF file");
+    }
+}
+
+// 判断节头表内部信息是否正常
+static void Check_Section(int fd, Elf32_Shdr sh_32, void *set){
+    assert(set != NULL);
+    assert(lseek(fd, (off_t)sh_32.sh_offset, SEEK_SET) == (off_t)sh_32.sh_offset);
+    assert(read(fd, set , sh_32.sh_size) == sh_32.sh_size);
+}
+
+// 读取节表信息 size, offset
+static void read_sheader(int fd, Elf32_Ehdr eh, Elf32_Shdr *sh_table){
+    assert(lseek(fd, eh.e_shoff, SEEK_SET) == eh.e_shoff);
+    for(int i = 0; i < eh.e_shnum; i ++){
+        assert(read(fd, (void *)&sh_table[i], eh.e_shentsize));
+    }
+}
+
+static void Read_Symble(int file, Elf32_Ehdr eh, Elf32_Shdr sh_table[], int sym_idx){
+    Elf32_Sym sym_table[sh_table[sym_idx].sh_size];
+    Check_Section(file, sh_table[sym_idx], sym_table);
+
+    int str_idx = sh_table[sym_idx].sh_link;
+    char str_table[sh_table[sym_idx].sh_size];
+    Check_Section(file, sh_table[str_idx], str_table);
+    int sym_count = (sh_table[sym_idx].sh_size / sizeof(Elf32_Sym));
+    ftrace_write("=======Functions in the symbol table.======\n\n");
+    for(int i = 0; i < sym_count; i++){
+        unsigned char type = ELF32_ST_TYPE(sym_table[i].st_info);
+        if(type == STT_FUNC) {
+            ftrace_write("0x%08x: call [%s@0x%08x]\n", (unsigned int)sym_table[i].st_value, &str_table[sym_table[i].st_name], (unsigned int)sym_table[i].st_value);
+            strncpy(fun_list[fun_index].fun_name, &str_table[sym_table[i].st_name], 31); // 使用 strncpy 来复制字符串
+            fun_list[fun_index].fun_name[31] = '\0'; // 确保字符串以 null 结尾
+            fun_list[fun_index++].fun_addr = (unsigned int)sym_table[i].st_value;
         }
     }
-    fclose(file);
+
+    for(int i = 0; i < fun_index; i++){
+        ftrace_write("%s ", fun_list[i].fun_name);
+    }
+
+    ftrace_write("\n=======The invocation of the function======\n");
+    S_table_size = sym_count;
+    S_table = malloc(sizeof(Stm_Table)* sym_count);
+    for(int i = 0; i < sym_count; i ++){
+        S_table[i].addr = sym_table[i].st_value;
+        S_table[i].info = sym_table[i].st_info;
+        S_table[i].size = sym_table[i].st_size;
+        memset(S_table[i].s_name, 0 , 32);
+    }
 }
-#endif
+
+static void read_sbols(int fd, Elf32_Ehdr eh, Elf32_Shdr sh_table[]){
+    for(int i = 0 ; i < eh.e_shnum; i ++){
+        switch(sh_table[i].sh_type){
+            case SHT_SYMTAB: case SHT_DYNSYM:
+            Read_Symble(fd, eh, sh_table, i);break;
+        }
+    }
+}
+
+static void init_funtail(){
+    fun_node_head = (FunNode *)malloc(sizeof(FunNode));
+    fun_node_head->pc = 0;
+    fun_node_head->next = NULL;
+}
+
+void init_elf(const char *elf_file){
+    if(elf_file == NULL){
+        return;
+    }
+    
+    int fd = open(elf_file, O_RDONLY|O_SYNC);
+    Elf32_Ehdr eh;
+    Read_ELFhead(fd, &eh);
+    
+    Elf32_Shdr sh_table[eh.e_shentsize * eh.e_shnum];
+    read_sheader(fd, eh, sh_table);//读取节头
+
+    read_sbols(fd, eh, sh_table);
+    init_funtail();//初始化记录函数的空节头
+
+    close(fd);
+}
+
+static int find_callfun(paddr_t target){
+    int i;
+    for(i = 0; i < fun_index; i ++){
+        if(fun_list[i].fun_addr == target){
+            // ftrace_write("=============dnpc:0x%08x\n", target);
+            break;
+        }
+    }
+    return i < S_table_size? i : -1;
+}
+
+void f_trace_call(paddr_t pc, paddr_t target){
+    if(S_table == NULL) return ;
+    ++Fun_depth;
+
+    int i = find_callfun(target);
+    char *fun_call_name;
+    if (i >= 0 && i < fun_index) {
+        fun_call_name = fun_list[i].fun_name; // 从 fun_list 中获取函数名
+    } else {
+        fun_call_name = "???"; // 如果找不到，就设为 "???"
+    }
+    ftrace_write("0x%08x: %*scall [%s@"FMT_PADDR "]\n",pc, Fun_depth, " ", fun_call_name, target);
+    push_fun_name(fun_call_name); // 将函数名压入栈
+    // 每次压入后输出一次全栈内容
+    
+}
+
+void f_trace_fun_ret(paddr_t pc){
+    if(S_table == NULL || fun_name_stack_top == 0) return;
+
+    char *fun_ret_name = pop_fun_name();
+    ftrace_write("0x%08x: %*sret  [%s]\n", pc, Fun_depth, " ", fun_ret_name);
+
+    --Fun_depth;
+    if(Fun_depth < 0) Fun_depth = 0;
+}
--- nemu/src/monitor/monitor.c
+++ nemu/src/monitor/monitor.c
@@ -18,6 +18,7 @@
 
 void init_rand();
 void init_log(const char *log_file);
+void init_elf(const char *elf_file);
 void init_mem();
 void init_difftest(char *ref_so_file, long img_size, int port);
 void init_device();
@@ -44,6 +45,8 @@ void sdb_set_batch_mode();
 static char *log_file = NULL;
 static char *diff_so_file = NULL;
 static char *img_file = NULL;
+static char *elf_file = NULL;
+static char *trace_file = NULL;
 static int difftest_port = 1234;
 
 static long load_img() {
@@ -72,8 +75,10 @@ static int parse_args(int argc, char *argv[]) {
   const struct option table[] = {
     {"batch"    , no_argument      , NULL, 'b'},
     {"log"      , required_argument, NULL, 'l'},
+    {"trace"    , required_argument, NULL, 't'},
     {"diff"     , required_argument, NULL, 'd'},
     {"port"     , required_argument, NULL, 'p'},
+    {"elf"      , required_argument, NULL, 'e'},
     {"help"     , no_argument      , NULL, 'h'},
     {0          , 0                , NULL,  0 },
   };
@@ -83,14 +88,18 @@ static int parse_args(int argc, char *argv[]) {
       case 'b': sdb_set_batch_mode(); break;
       case 'p': sscanf(optarg, "%d", &difftest_port); break;
       case 'l': log_file = optarg; break;
+      case 't': trace_file = optarg; break;
       case 'd': diff_so_file = optarg; break;
+      case 'e': elf_file = optarg; break;
       case 1: img_file = optarg; return 0;
       default:
         printf("Usage: %s [OPTION...] IMAGE [args]\n\n", argv[0]);
         printf("\t-b,--batch              run with batch mode\n");
         printf("\t-l,--log=FILE           output log to FILE\n");
+        printf("\t-t,--trace=FILE         output trace to FILE\n");
         printf("\t-d,--diff=REF_SO        run DiffTest with reference REF_SO\n");
         printf("\t-p,--port=PORT          run DiffTest with port PORT\n");
+        printf("\t-e,--elf=FILE           elf file fail\n");
         printf("\n");
         exit(0);
     }
@@ -112,6 +121,10 @@ void init_monitor(int argc, char *argv[]) {
 
   /* Initialize memory. */
   init_mem();
+
+#ifdef CONFIG_FTRACE
+  init_elf(elf_file);
+#endif
 
   /* Initialize devices. */
   IFDEF(CONFIG_DEVICE, init_device());
