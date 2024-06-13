b535131a93ef6a3eadcc4bcf164f7b30d0413ed4
tracer-ics2023 <tracer@njuics.org>
2024-06-13 14:24:25 +0800

    >  compile NEMU 11451400 lemon Linux lemonsuqing 6.5.0-35-generic #35~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue May  7 09:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:24:25 up 3 min,  1 user,  load average: 0.66, 0.54, 0.23

--- abstract-machine/am/src/platform/nemu/ioe/input.c
+++ abstract-machine/am/src/platform/nemu/ioe/input.c
@@ -3,24 +3,13 @@
 
 #define KEYDOWN_MASK 0x8000
 
-#define KEY_NUM 256
-
-static int key_state[KEY_NUM];
-
-void update_key_state(int keycode, int down) {
-  key_state[keycode] = down;
-}
-
-int is_key_down(int keycode) {
-  return key_state[keycode];
-}
-
 void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
   uint32_t k = inl(KBD_ADDR);
-  int down = (k & KEYDOWN_MASK) != 0;
-  int keycode = down ? (k & ~KEYDOWN_MASK) : AM_KEY_NONE;
-  update_key_state(keycode, down);
-  kbd->keydown = down;
-  kbd->keycode = keycode;
+  if(k & KEYDOWN_MASK){
+    kbd->keydown = true;
+    kbd->keycode = k & ~KEYDOWN_MASK;
+  }else{
+    kbd->keydown = false;
+    kbd->keycode = AM_KEY_NONE;
+  }
 }
-
