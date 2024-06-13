55b21fd8a381f5c43cf7108c81975255d9e7eb08
tracer-ics2023 <tracer@njuics.org>
2024-06-13 23:20:42 +0800

    >  compile NEMU 11451400 lemon Linux lemonsuqing 6.5.0-35-generic #35~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue May  7 09:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:20:42 up  2:06,  1 user,  load average: 0.11, 0.14, 0.17

--- abstract-machine/scripts/platform/nemu.mk
+++ abstract-machine/scripts/platform/nemu.mk
@@ -13,7 +13,7 @@ LDFLAGS   += -T $(AM_HOME)/scripts/linker.ld \
 LDFLAGS   += --gc-sections -e _start
 NEMUFLAGS += -l $(shell dirname $(IMAGE).elf)/nemu-log.txt
 NEMUFLAGS += -e $(IMAGE).elf
-NEMUFLAGS += -t $(shell dirname $(IMAGE).elf)/trace-log.txt
+# NEMUFLAGS += -t $(shell dirname $(IMAGE).elf)/trace-log.txt
 NEMUFLAGS += -b
 CFLAGS += -DMAINARGS=\"$(mainargs)\"
 CFLAGS += -I$(AM_HOME)/am/src/platform/nemu/include
