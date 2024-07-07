5364d1e057b28cde41ebbabc244081d964be7d5f
tracer-ics2023 <tracer@njuics.org>
2024-07-07 21:02:45 +0800

    >  compile NEMU 11451400 lemon Linux lemonsuqing 6.5.0-35-generic #35~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue May  7 09:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:02:45 up 1 day, 17:40,  1 user,  load average: 0.54, 0.35, 0.26

--- nemu/src/isa/riscv32/include/isa-def.h
+++ nemu/src/isa/riscv32/include/isa-def.h
@@ -18,9 +18,18 @@
 
 #include <common.h>
 
+typedef struct CSR_Reg{
+  word_t mepc;
+  word_t mstatus;
+  word_t mcause;
+  word_t mtvec;
+}CSR_Reg;
+
+
 typedef struct {
   word_t gpr[MUXDEF(CONFIG_RVE, 16, 32)];
   vaddr_t pc;
+  CSR_Reg csr;
 } MUXDEF(CONFIG_RV64, riscv64_CPU_state, riscv32_CPU_state);
 
 // decode
