679b14e1f8f15af58d3fb84c916ddd9d42d8fc0e
tracer-ics2023 <tracer@njuics.org>
2024-07-09 01:57:10 +0800

    >  compile NEMU 11451400 lemon Linux lemonsuqing 6.5.0-35-generic #35~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue May  7 09:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  01:57:10 up 2 days, 45 min,  1 user,  load average: 0.40, 0.28, 0.23

--- nemu/src/isa/riscv32/system/intr.c
+++ nemu/src/isa/riscv32/system/intr.c
@@ -19,11 +19,10 @@ word_t isa_raise_intr(word_t NO, vaddr_t epc) {
   /* TODO: Trigger an interrupt/exception with ``NO''.
    * Then return the address of the interrupt/exception vector.
    */
-  printf("isa_raise_intr\n");
 
-  if(NO==0){
-    epc+=4;
-  }
+  // if(NO==0){
+  //   epc+=4;
+  // }
   // cpu.csr.mstatus = 0x1800;
   cpu.csr.mcause = NO;   // 设置异常发生的原因
   cpu.csr.mepc = epc;    // 存放触发异常的pc值
