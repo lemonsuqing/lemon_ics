f2989f81dc885865b5ddbdca410d49b37e40a50a
tracer-ics2023 <tracer@njuics.org>
2024-07-09 01:39:28 +0800

    >  compile NEMU 11451400 lemon Linux lemonsuqing 6.5.0-35-generic #35~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue May  7 09:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  01:39:28 up 2 days, 27 min,  1 user,  load average: 0.13, 0.21, 0.25

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -9,8 +9,10 @@ Context* __am_irq_handle(Context *c) {
     Event ev = {0};
     switch (c->mcause) {
       case 0:
-        ev.event=EVENT_YIELD;break;
-      default: ev.event = EVENT_ERROR; break;
+        // ev.event=EVENT_YIELD;
+        break;
+      default:// ev.event = EVENT_ERROR;
+        break;
     }
 
     c = user_handler(ev, c);
