8c719b11bfc5eea3ffcde24e7f3e899546af4774
tracer-ics2023 <tracer@njuics.org>
2024-07-11 12:37:15 +0800

    >  compile NEMU 11451400 lemon Linux lemonsuqing 6.5.0-35-generic #35~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue May  7 09:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  12:37:15 up 2 days,  9:42,  1 user,  load average: 0.08, 0.14, 0.17

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -11,15 +11,14 @@
 
 static uintptr_t loader(PCB *pcb, const char *filename) {
   Elf_Ehdr ehdr;// ELF文件头
-  printf("123\n");
   // 可执行文件位于ramdisk偏移为0处，访问它就可以得到用户程序的第一个字节
   ramdisk_read(&ehdr, 0, sizeof(Elf_Ehdr));
   assert((*(uint32_t *)ehdr.e_ident == 0x464c457f));
-  printf("TYPE:%d Phum:%d\n",ehdr.e_type, ehdr.e_phnum);
+//  printf("TYPE:%d Phum:%d\n",ehdr.e_type, ehdr.e_phnum);
 
   Elf_Phdr phdr[ehdr.e_phnum];// 程序头表
   ramdisk_read(phdr, ehdr.e_phoff, sizeof(Elf_Phdr)*ehdr.e_phnum);
-  printf("TYPE:%d Phum:%d\n",phdr->p_type, phdr->p_memsz);
+//  printf("TYPE:%d Phum:%d\n",phdr->p_type, phdr->p_memsz);
   for (int i = 0; i < ehdr.e_phnum; i++) {
     if (phdr[i].p_type == PT_LOAD) {
       ramdisk_read((void*)phdr[i].p_vaddr, phdr[i].p_offset, phdr[i].p_memsz);
