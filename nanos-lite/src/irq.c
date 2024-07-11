6a8c620c6fe4dc28fcb2e2aa81a003dccc2dd655
tracer-ics2023 <tracer@njuics.org>
2024-07-11 22:52:15 +0800

    >  compile NEMU 11451400 lemon Linux lemonsuqing 6.5.0-35-generic #35~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue May  7 09:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:52:15 up 2 days, 13:48,  1 user,  load average: 0.44, 0.27, 0.21

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -3,8 +3,8 @@
 static Context* do_event(Event e, Context* c) {
   printf("do_event's e.event=%d\n",e.event);
   switch (e.event) {
-    case 1:
-      printf("e.event = 1\n");break;
+    case 1:// yeild
+      printf("yeild: e.event = 1\n");break;
     case 2:do_syscall(c);break;//EVENT_SYSCALL
     default: panic("Unhandled event ID = %d", e.event);
   }
