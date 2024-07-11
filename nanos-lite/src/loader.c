77dabf55ddef90f53ce9b42e73a4fe9f90c81ba6
tracer-ics2023 <tracer@njuics.org>
2024-07-11 11:16:07 +0800

    >  compile NEMU 11451400 lemon Linux lemonsuqing 6.5.0-35-generic #35~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue May  7 09:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:16:07 up 2 days,  8:21,  1 user,  load average: 0.43, 0.28, 0.21

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -10,11 +10,12 @@
 #endif
 
 static uintptr_t loader(PCB *pcb, const char *filename) {
-  Elf_Ehdr ehdr;
+  Elf_Ehdr ehdr;// ELF文件头
+  // 可执行文件位于ramdisk偏移为0处，访问它就可以得到用户程序的第一个字节
   ramdisk_read(&ehdr, 0, sizeof(Elf_Ehdr));
   assert((*(uint32_t *)ehdr.e_ident == 0x464c457f));
-
-  Elf_Phdr phdr[ehdr.e_phnum];
+  printf("TYPE %s \n",ehdr.e_type);
+  Elf_Phdr phdr[ehdr.e_phnum];// 程序头表
   ramdisk_read(phdr, ehdr.e_phoff, sizeof(Elf_Phdr)*ehdr.e_phnum);
   for (int i = 0; i < ehdr.e_phnum; i++) {
     if (phdr[i].p_type == PT_LOAD) {
