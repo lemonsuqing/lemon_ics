648ec0a00078da52c2b57d7a3fd7f889a69243a2
tracer-ics2023 <tracer@njuics.org>
2024-07-11 22:48:02 +0800

    >  compile NEMU 11451400 lemon Linux lemonsuqing 6.5.0-35-generic #35~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue May  7 09:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:48:02 up 2 days, 13:44,  1 user,  load average: 0.18, 0.17, 0.18

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -6,7 +6,7 @@ void do_syscall(Context *c) {
 
   switch (a[0]) {
     case 0:c->GPRx=0;printf("SYS_exit， do_syscall此时 c->GPRx=%d\n",c->GPRx);halt(c->GPRx);//SYS_exit系统调用
-    case 1:printf("SYS_yield， do_syscall此时c->GPRx=%d\n",c->GPRx);yield(); //SYS_yield系统调用
+    case 1:printf("SYS_yield， do_syscall此时c->GPRx=%d\n",c->GPRx);yield();break; //SYS_yield系统调用
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
 }
