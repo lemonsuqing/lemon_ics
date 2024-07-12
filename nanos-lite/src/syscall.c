98f2b2b04fdbb1c86be5e049409d48b0d3bfc656
tracer-ics2023 <tracer@njuics.org>
2024-07-12 21:33:55 +0800

    >  compile NEMU 11451400 lemon Linux lemonsuqing 6.5.0-35-generic #35~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue May  7 09:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:33:55 up 2 days, 21:24,  1 user,  load average: 0.65, 0.34, 0.23

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,5 +1,14 @@
 #include <common.h>
 #include "syscall.h"
+
+
+void SYS_write(intptr_t *buf, size_t count){
+      for (int i = 0; i < count; i++) {
+    putch(*((char*)buf + i));
+  }
+}
+
+
 void do_syscall(Context *c) {
   uintptr_t a[4];
   a[0] = c->GPR1;//存储系统调用的类型
@@ -11,7 +20,7 @@ void do_syscall(Context *c) {
     case 1:// SYS_yield
       yield();break;
     case 4:// SYS_write
-      
+      SYS_write((intptr_t *)c->GPR2,c->GPR3);
       break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
