b1fdbe93046e2972317a875d49a10b704ef8db44
tracer-ics2023 <tracer@njuics.org>
2024-07-15 00:43:56 +0800

    >  compile NEMU 11451400 lemon Linux lemonsuqing 6.5.0-41-generic #41~22.04.2-Ubuntu SMP PREEMPT_DYNAMIC Mon Jun  3 11:32:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  00:43:56 up 10:25,  1 user,  load average: 0.39, 0.28, 0.28

--- navy-apps/libs/libndl/NDL.c
+++ navy-apps/libs/libndl/NDL.c
@@ -63,9 +63,9 @@ int NDL_Init(uint32_t flags) {
   if (getenv("NWM_APP")) {
     evtdev = 3;
   }
-  if(flags == 3){
-    evtdev = 3;
-  }
+  // if(flags == 3){
+  //   evtdev = 3;
+  // }
 
   return 0;
 }
