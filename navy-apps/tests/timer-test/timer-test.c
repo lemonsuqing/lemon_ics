bf6e9a92b71c589ba60a09f469ffd05f100e1c80
tracer-ics2023 <tracer@njuics.org>
2024-07-14 18:01:24 +0800

    >  compile NEMU 11451400 lemon Linux lemonsuqing 6.5.0-41-generic #41~22.04.2-Ubuntu SMP PREEMPT_DYNAMIC Mon Jun  3 11:32:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:01:24 up  3:42,  1 user,  load average: 0.72, 0.60, 0.49

--- navy-apps/tests/timer-test/timer-test.c
+++ navy-apps/tests/timer-test/timer-test.c
@@ -5,18 +5,18 @@
 int main() {
     struct timeval tv;
     struct timezone tz;
-    time_t last_time = 0;
     time_t last_half_second_time = 0;
+    int num = 0;
 
     while (1) {
         gettimeofday(&tv, &tz);
-        double time_interval = (double)(tv.tv_sec - last_time);
         double half_second_interval = (double)(tv.tv_sec - last_half_second_time);
-        if (time_interval >= 1.0) {
-            printf("Now Time: %lds, %ldus\n", tv.tv_sec, tv.tv_usec);
-            last_time = tv.tv_sec;
-        }
         if (half_second_interval >= 0.5) {
+            num++;
+            if(num >= 2){
+                num = 0;
+                printf("Now Time: %lds, %ldus\n", tv.tv_sec, tv.tv_usec);
+            }
             printf("0.5 seconds later\n");
             last_half_second_time = tv.tv_sec;
         }
