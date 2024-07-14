a89b61e476007e0b3995abd5339382b59095cbb9
tracer-ics2023 <tracer@njuics.org>
2024-07-14 17:16:15 +0800

    >  compile NEMU 11451400 lemon Linux lemonsuqing 6.5.0-41-generic #41~22.04.2-Ubuntu SMP PREEMPT_DYNAMIC Mon Jun  3 11:32:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:16:15 up  2:57,  1 user,  load average: 0.30, 0.37, 0.33

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -35,7 +35,7 @@ void do_syscall(Context *c) {
       c->GPRx=0;
       break;
     case SYS_gettimeofday:
-      sys_gettimeofday((struct timeval *)(c->GPR2),(struct timezone *)(c->GPR3));
+      c->GPRx = sys_gettimeofday((struct timeval *)(c->GPR2),(struct timezone *)(c->GPR3));
       break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
