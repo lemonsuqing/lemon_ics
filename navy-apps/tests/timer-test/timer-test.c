44921cafed5aed6d0a2fa0043cd9a3546d1f2a0a
tracer-ics2023 <tracer@njuics.org>
2024-07-14 21:01:51 +0800

    >  compile NEMU 11451400 lemon Linux lemonsuqing 6.5.0-41-generic #41~22.04.2-Ubuntu SMP PREEMPT_DYNAMIC Mon Jun  3 11:32:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:01:51 up  6:43,  1 user,  load average: 0.28, 0.28, 0.28

--- navy-apps/tests/timer-test/timer-test.c
+++ navy-apps/tests/timer-test/timer-test.c
@@ -1,7 +1,25 @@
 #include <stdio.h>
 #include <sys/time.h>
 #include <time.h>
+#include <NDL.h>
 
+int main(){
+    NDL_Init(0);
+    uint32_t sec = 0;
+    uint32_t usec = 0;
+    while(1) {
+    while(usec/100000 < sec){
+      usec = NDL_GetTicks();
+      printf("usec is %d\n",usec);
+    }
+    usec = NDL_GetTicks();
+    printf("some setence~~\n----------------\n");
+    sec += 5;
+  }
+    return 0;
+}
+
+/*
 int main() {
     struct timeval tv;
     struct timezone tz;
@@ -19,3 +37,4 @@ int main() {
 
     return 0;
 }
+*/
