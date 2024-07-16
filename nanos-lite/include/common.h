51028dd794d293b0b48595658e2ad0c6788c909c
tracer-ics2023 <tracer@njuics.org>
2024-07-16 21:02:48 +0800

    >  compile NEMU 11451400 lemon Linux lemonsuqing 6.5.0-41-generic #41~22.04.2-Ubuntu SMP PREEMPT_DYNAMIC Mon Jun  3 11:32:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:02:48 up  2:30,  1 user,  load average: 0.71, 0.86, 0.59

--- nanos-lite/include/common.h
+++ nanos-lite/include/common.h
@@ -24,5 +24,7 @@ int fs_close(int fd);
 // device
 size_t serial_write(const void *buf, size_t offset, size_t len);
 size_t events_read(void *buf, size_t offset, size_t len);
+size_t dispinfo_read(void *buf, size_t offset, size_t len);
+size_t fb_write(const void *buf, size_t offset, size_t len);
 
 #endif
