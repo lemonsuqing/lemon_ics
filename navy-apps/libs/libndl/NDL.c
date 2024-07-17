921d71f54882ef667406774ed663f7c7f4fea541
tracer-ics2023 <tracer@njuics.org>
2024-07-18 00:11:10 +0800

    >  compile NEMU 11451400 lemon Linux lemonsuqing 6.5.0-41-generic #41~22.04.2-Ubuntu SMP PREEMPT_DYNAMIC Mon Jun  3 11:32:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  00:11:10 up  4:43,  1 user,  load average: 0.35, 0.33, 0.29

--- navy-apps/libs/libndl/NDL.c
+++ navy-apps/libs/libndl/NDL.c
@@ -70,13 +70,11 @@ void NDL_OpenCanvas(int *w, int *h) {
 
 //解析 /proc/dispinfo 文件的内容，并写入 screen_w 和 screen_h，作为屏幕大小：
 static void init_dispinfo() {
-  printf("in init_dispinfio\n");
   int buf_size = 1024;
   char * buf = (char *)malloc(buf_size * sizeof(char));
   int fd = open("/proc/dispinfo", 0, 0);
-  printf("init_dispinfio: fd = %d\n", fd);
   int ret = read(fd, buf, buf_size);
-  printf("init_dispinfio: buf = %s\n", buf);
+  // printf("init_dispinfio: buf = %s\n", buf);
   assert(ret < buf_size); // to be cautious...
   assert(close(fd) == 0);
 
