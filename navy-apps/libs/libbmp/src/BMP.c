7503ffced0690fba880f2cdc181db4d117594546
tracer-ics2023 <tracer@njuics.org>
2024-07-18 01:18:48 +0800

    >  compile NEMU 11451400 lemon Linux lemonsuqing 6.5.0-41-generic #41~22.04.2-Ubuntu SMP PREEMPT_DYNAMIC Mon Jun  3 11:32:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  01:18:48 up  5:51,  1 user,  load average: 0.66, 0.55, 0.47

--- navy-apps/libs/libbmp/src/BMP.c
+++ navy-apps/libs/libbmp/src/BMP.c
@@ -43,9 +43,9 @@ void* BMP_Load(const char *filename, int *width, int *height) {
       pixels[w * i + j] = (r << 16) | (g << 8) | b;
     }
   }
-  printf("3\n");
   fclose(fp);
   if (width) *width = w;
   if (height) *height = h;
+  printf("4\n");
   return pixels;
 }
