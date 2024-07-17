94fc45a1f2c3f0b6e00ccee952f23ac4f85ee88e
tracer-ics2023 <tracer@njuics.org>
2024-07-18 01:41:54 +0800

    >  compile NEMU 11451400 lemon Linux lemonsuqing 6.5.0-41-generic #41~22.04.2-Ubuntu SMP PREEMPT_DYNAMIC Mon Jun  3 11:32:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  01:41:54 up  6:14,  1 user,  load average: 0.52, 0.43, 0.44

--- navy-apps/libs/libos/src/syscall.c
+++ navy-apps/libs/libos/src/syscall.c
@@ -65,7 +65,7 @@ int _open(const char *path, int flags, mode_t mode) {
 }
 
 int _write(int fd, void *buf, size_t count) {
-  assert(fd == 1 || fd == 2);
+
   _syscall_(SYS_write, fd, (intptr_t)buf, count);
   
   return count;
