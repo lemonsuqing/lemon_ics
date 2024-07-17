7cc9af6ffe39a73592824246d1f9fc5f8214c162
tracer-ics2023 <tracer@njuics.org>
2024-07-18 01:31:12 +0800

    >  compile NEMU 11451400 lemon Linux lemonsuqing 6.5.0-41-generic #41~22.04.2-Ubuntu SMP PREEMPT_DYNAMIC Mon Jun  3 11:32:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  01:31:12 up  6:03,  1 user,  load average: 0.41, 0.54, 0.50

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -50,7 +50,7 @@ size_t fb_write(const void *buf, size_t offset, size_t len) {
   len /= 4;
  
   int y = offset / width;
-  int x = offset - y * width;
+  int x = offset % width;
  
   io_write(AM_GPU_FBDRAW, x, y, (void *)buf, len, 1, true);
  
