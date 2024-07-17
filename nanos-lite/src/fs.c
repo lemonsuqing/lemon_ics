80405bf0f8955abac4b8cb122776b306cf39f27a
tracer-ics2023 <tracer@njuics.org>
2024-07-18 00:55:06 +0800

    >  compile NEMU 11451400 lemon Linux lemonsuqing 6.5.0-41-generic #41~22.04.2-Ubuntu SMP PREEMPT_DYNAMIC Mon Jun  3 11:32:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  00:55:06 up  5:27,  1 user,  load average: 0.60, 0.42, 0.38

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -50,6 +50,7 @@ int fs_open(const char *pathname, int flags, int mode){
   for (int i = 0; i < NR_FILES; i++) {
         if (strcmp(file_table[i].name, pathname) == 0) {
             file_table[i].open_offset = 0;
+            printf("filename %s, num %d", pathname, i);
             return i;
         }
     }
