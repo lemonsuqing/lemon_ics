cfd5250b70f9f46d04dcfae9eb9e3d2ca357c4b4
tracer-ics2023 <tracer@njuics.org>
2024-07-20 18:07:47 +0800

    >  compile NEMU 11451400 lemon Linux lemonsuqing 6.5.0-41-generic #41~22.04.2-Ubuntu SMP PREEMPT_DYNAMIC Mon Jun  3 11:32:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:07:47 up 13:58,  1 user,  load average: 0.20, 0.41, 0.40

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -43,20 +43,16 @@ size_t dispinfo_read(void *buf, size_t offset, size_t len) {
 }
 
 size_t fb_write(const void *buf, size_t offset, size_t len) {
-  AM_GPU_CONFIG_T ev = io_read(AM_GPU_CONFIG);
-  int width = ev.width;
- 
-  offset /= 4;
-  len /= 4;
- 
-  int y = offset / width;
-  int x = offset % width;
-
-  printf("x:%d  y:%d\n",x,y);
- 
-  io_write(AM_GPU_FBDRAW, x, y, (void *)buf, len, 1, true);
- 
-  return len;
+  //printf("offset : %d, len : %d\n", offset, len);
+  AM_GPU_FBDRAW_T fb_ctl;
+  fb_ctl.pixels = (uint32_t *)buf;
+  fb_ctl.x = offset % 400;
+  fb_ctl.y = offset / 400;
+  fb_ctl.w = len, fb_ctl.h = 1;
+  fb_ctl.sync = true;
+ // printf("x : %d, y : %d, w : %d, h: %d\n",fb_ctl.x, fb_ctl.y, fb_ctl.w, fb_ctl.h);
+  ioe_write(AM_GPU_FBDRAW, &fb_ctl);
+  return 0;
 }
 
 void init_device() {
