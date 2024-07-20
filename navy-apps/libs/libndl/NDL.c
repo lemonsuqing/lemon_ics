d4a268a1401cefdb695764bee4c4c012277fb27e
tracer-ics2023 <tracer@njuics.org>
2024-07-20 22:05:04 +0800

    >  compile NEMU 11451400 lemon Linux lemonsuqing 6.5.0-41-generic #41~22.04.2-Ubuntu SMP PREEMPT_DYNAMIC Mon Jun  3 11:32:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:05:04 up 17:55,  1 user,  load average: 0.36, 0.37, 0.50

--- navy-apps/libs/libndl/NDL.c
+++ navy-apps/libs/libndl/NDL.c
@@ -66,8 +66,8 @@ void NDL_OpenCanvas(int *w, int *h) {
     canvas_w = *w;
     canvas_h = *h;
   }
-  canvas_x=(screen_w - canvas_w) / 2;
-  canvas_y=(screen_h - canvas_h) / 2;
+  canvas_x = (screen_w - canvas_w) / 2;
+  canvas_y = (screen_h - canvas_h) / 2;
   printf("画布的大小为宽%d X 高%d\n",canvas_w,canvas_h);
   printf("相对于屏幕左上角的画布位置坐标x:%d,y:%d\n",canvas_x,canvas_y);
 }
@@ -102,9 +102,11 @@ static void init_dispinfo() {
 
 void NDL_DrawRect(uint32_t *pixels, int x, int y, int w, int h) {
   int fd = open("/dev/fb", 0, 0);
+  x += canvas_x;
   size_t offset = 0;
   for (int i = 0; i < h && y + i < canvas_h; ++i) {
-    offset += ((y + canvas_y) * screen_w + (x + canvas_x)) * 4;
+
+    offset += ((y + canvas_y) * screen_w + (x + screen_w)) * 4;
     // printf("offset: %ld\tn: %d\tfd:%d\n", offset, 4*(w < canvas_w - x ? w : canvas_w - x), fd);
     lseek(fd, offset, SEEK_SET);
     write(fd, pixels + i * w, 4 * (w < canvas_w - x ? w : canvas_w - x));
