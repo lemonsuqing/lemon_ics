067b6d83181fa2241bdf5393dac8175933b4fa78
tracer-ics2023 <tracer@njuics.org>
2024-07-20 16:09:39 +0800

    >  compile NEMU 11451400 lemon Linux lemonsuqing 6.5.0-41-generic #41~22.04.2-Ubuntu SMP PREEMPT_DYNAMIC Mon Jun  3 11:32:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:09:39 up 12:00,  1 user,  load average: 0.58, 0.42, 0.31

--- navy-apps/libs/libndl/NDL.c
+++ navy-apps/libs/libndl/NDL.c
@@ -99,7 +99,7 @@ static void init_dispinfo() {
 
 void NDL_DrawRect(uint32_t *pixels, int x, int y, int w, int h) {
   int fd = open("/dev/fb", 0, 0);
-  x = -100;y = -50;
+  x = -400;y = -300;
   for (int i = 0; i < h && y + i < canvas_h; ++i) {
     lseek(fd, ((y + canvas_y + i) * screen_w + (x + canvas_x)) * 4, SEEK_SET);
     write(fd, pixels + i * w, 4 * (w < canvas_w - x ? w : canvas_w - x));
