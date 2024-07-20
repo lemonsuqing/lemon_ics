ac93b0ed01e227824501185481e22b7d31ac0d71
tracer-ics2023 <tracer@njuics.org>
2024-07-20 20:54:26 +0800

    >  compile NEMU 11451400 lemon Linux lemonsuqing 6.5.0-41-generic #41~22.04.2-Ubuntu SMP PREEMPT_DYNAMIC Mon Jun  3 11:32:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:54:26 up 16:45,  1 user,  load average: 0.16, 0.21, 0.22

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -106,7 +106,7 @@ size_t fs_lseek(int fd, size_t offset, int whence){
         Log("ignore lseek %s", file_table[fd].name);
         return 0;
   }
-  printf("fs==> offset: %d \t file_table[%d].size: %d\n",offset, fd, file_table[fd].size);
+  // printf("fs==> offset: %d \t file_table[%d].size: %d\n",offset, fd, file_table[fd].size);
   Finfo *file = &file_table[fd];
   size_t new_offset;
   /*
