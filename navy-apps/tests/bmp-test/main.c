a0195536030f6762e724df6f444e762cea8ed9d2
tracer-ics2023 <tracer@njuics.org>
2024-07-20 16:24:36 +0800

    >  compile NEMU 11451400 lemon Linux lemonsuqing 6.5.0-41-generic #41~22.04.2-Ubuntu SMP PREEMPT_DYNAMIC Mon Jun  3 11:32:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:24:36 up 12:15,  1 user,  load average: 0.28, 0.25, 0.26

--- navy-apps/tests/bmp-test/main.c
+++ navy-apps/tests/bmp-test/main.c
@@ -9,15 +9,11 @@ int main() {
   int w, h;
   void *bmp = BMP_Load("/share/pictures/projectn.bmp", &w, &h);
   assert(bmp);
-  int canvas_w, canvas_h;
-  NDL_OpenCanvas(&canvas_w, &canvas_h);
-  int center_x = (canvas_w - w) / 2;
-  int center_y = (canvas_h - h) / 2;
-  NDL_DrawRect(bmp, center_x, center_y, w, h);
+  NDL_OpenCanvas(&w, &h);
+  NDL_DrawRect(bmp, 0, 0, w, h);
   free(bmp);
   NDL_Quit();
   printf("Test ends! Spinning...\n");
   while (1);
   return 0;
 }
-
