3a07e99efdefdf2d5d6d59d1aa3cc3295f1e8aea
tracer-ics2023 <tracer@njuics.org>
2024-07-26 00:33:05 +0800

    >  compile NEMU 11451400 lemon Linux lemonsuqing 6.5.0-44-generic #44~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 18 14:36:16 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  00:33:05 up  3:52,  1 user,  load average: 0.51, 0.45, 0.38

--- navy-apps/libs/libminiSDL/src/event.c
+++ navy-apps/libs/libminiSDL/src/event.c
@@ -12,61 +12,12 @@ int SDL_PushEvent(SDL_Event *ev) {
   return 0;
 }
 
-int SDL_PollEvent(SDL_Event *event) {
-  unsigned buf_size = 32;
-  char *buf = (char *)malloc(buf_size * sizeof(char));
-  if (NDL_PollEvent(buf, buf_size) == 1) {
-      if (strncmp(buf, "kd", 2) == 0) {
-          event->key.type = SDL_KEYDOWN;
-      } else {
-          event->key.type = SDL_KEYUP;
-      }
-
-      int flag = 0;
-      for (unsigned i = 0; i < sizeof(keyname) / sizeof(keyname[0]); ++i) {
-          if (strncmp(buf + 3, keyname[i], strlen(buf) - 4) == 0
-                  && strlen(keyname[i]) == strlen(buf) - 4) {
-              flag = 1;
-              event->key.keysym.sym = i;
-              break;
-          }
-      }
-
-      assert(flag == 1);
-
-      free(buf);
-      return 1;
-  } else {
-      return 0;
-  }
+int SDL_PollEvent(SDL_Event *ev) {
+  return 0;
 }
 
 int SDL_WaitEvent(SDL_Event *event) {
-  unsigned buf_size = 32;
-  char *buf = (char *)malloc(buf_size * sizeof(char));
-
-    while (NDL_PollEvent(buf, buf_size) == 0); 
-
-    if (strncmp(buf, "kd", 2) == 0)
-        event->key.type = SDL_KEYDOWN;
-    else
-        event->key.type = SDL_KEYUP;
-
-
-    int flag = 0;
-    for (unsigned i = 0; i < sizeof(keyname) / sizeof(keyname[0]); ++i) {
-        if (strncmp(buf + 3, keyname[i], strlen(buf) - 4) == 0
-                && strlen(keyname[i]) == strlen(buf) - 4) {
-            flag = 1;
-            event->key.keysym.sym = i;
-            break;
-        }
-    }
-
-    assert(flag == 1);
-
-    free(buf);
-    return 1;
+  return 0;
 }
 
 int SDL_PeepEvents(SDL_Event *ev, int numevents, int action, uint32_t mask) {
