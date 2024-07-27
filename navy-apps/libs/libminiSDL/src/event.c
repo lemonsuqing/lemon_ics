1ad57c0687e6ed19c7cd406fad39a51d8a441292
tracer-ics2023 <tracer@njuics.org>
2024-07-27 21:27:13 +0800

    >  compile NEMU 11451400 lemon Linux lemonsuqing 6.5.0-44-generic #44~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 18 14:36:16 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:27:13 up 13:58,  1 user,  load average: 0.67, 0.51, 0.50

--- navy-apps/libs/libminiSDL/src/event.c
+++ navy-apps/libs/libminiSDL/src/event.c
@@ -2,47 +2,112 @@
 #include <SDL.h>
 #include <stdlib.h>
 #include <string.h>
+#include <assert.h>
 
 #define keyname(k) #k,
-
+static uint8_t key_state[83] = {0};
 static const char *keyname[] = {
   "NONE",
   _KEYS(keyname)
 };
+
+int parse_event(uint8_t *_key, uint8_t *_type){
+  char buf[60], buf_key[30];
+  int len = sizeof(buf);
+  int _len = NDL_PollEvent(buf, len);
+  int i;
+  uint8_t key = SDLK_NONE, type = SDL_KEYUP;
+  if(_len){
+    char * toke0 = strtok(buf, " ");
+    char * toke1 = strtok(NULL, " ");
+    switch(toke1[0]){
+      case 'k':
+        switch(toke1[1]){
+          case 'u':
+            type = SDL_KEYUP;
+            break;
+          case 'd':
+            type = SDL_KEYDOWN;
+            break;
+          default:
+            printf("%s\n", toke1[1]);
+            assert(0);
+        }
+        break;
+      default:
+        printf("%s\n", toke1[0]);
+        assert(0);
+    }
+    sscanf(toke0, "%s", buf_key);
+    for (i = 0; i < sizeof(keyname) / sizeof(char*); ++ i) {
+      if (strcmp(buf_key, keyname[i]) == 0) {
+        key = i;
+        break;
+      }
+    }
+    if(key){
+      *_key = key;
+      *_type = type;
+      return 1;
+    }else{
+      printf("unknow key %s\n", toke0);
+      assert(0);
+    }
+    return 0;
+  }else{
+      if (_key) *_key = SDLK_NONE;
+      if (_type) *_type = SDL_KEYUP;
+      return 0;
+  }
+}
 
 int SDL_PushEvent(SDL_Event *ev) {
   return 0;
 }
 
 int SDL_PollEvent(SDL_Event *ev) {
-  unsigned buf_size = 32;
-  char *buf = (char *)malloc(buf_size * sizeof(char));
-  if (NDL_PollEvent(buf, buf_size) == 1) {
-      if (strncmp(buf, "kd", 2) == 0) {
-          ev->key.type = SDL_KEYDOWN;
-      } else {
-          ev->key.type = SDL_KEYUP;
-      }
+  SDL_Init(3);
+  uint8_t key, type;
+  int ret = parse_event(&key, &type);
+  ev->type = type;
+  ev->key.type = type;
+  ev->key.keysym.sym = key;
+  if (type == SDL_KEYDOWN)
+    key_state[key] = 1;
+  else if (ev->type == SDL_KEYUP)
+    key_state[key] = 0;
+  return ret;
+}
 
-      int flag = 0;
-      for (unsigned i = 0; i < sizeof(keyname) / sizeof(keyname[0]); ++i) {
-        printf("i = %d\n",i);
-          if (strncmp(buf + 3, keyname[i], strlen(buf) - 4) == 0
-                  && strlen(keyname[i]) == strlen(buf) - 4) {
-              flag = 1;
-              printf("ev->key.keysym.sym: %d\n", i);
-              ev->key.keysym.sym = i;
-              break;
-          }
-      }
+// int SDL_PollEvent(SDL_Event *ev) {
+//   unsigned buf_size = 32;
+//   char *buf = (char *)malloc(buf_size * sizeof(char));
+//   if (NDL_PollEvent(buf, buf_size) == 1) {
+//       if (strncmp(buf, "kd", 2) == 0) {
+//           ev->key.type = SDL_KEYDOWN;
+//       } else {
+//           ev->key.type = SDL_KEYUP;
+//       }
+
+//       int flag = 0;
+//       for (unsigned i = 0; i < sizeof(keyname) / sizeof(keyname[0]); ++i) {
+//         printf("i = %d\n",i);
+//           if (strncmp(buf + 3, keyname[i], strlen(buf) - 4) == 0
+//                   && strlen(keyname[i]) == strlen(buf) - 4) {
+//               flag = 1;
+//               printf("ev->key.keysym.sym: %d\n", i);
+//               ev->key.keysym.sym = i;
+//               break;
+//           }
+//       }
 
       
-      free(buf);
-      return 1;
-  } else {
-      return 0;
-  }
-}
+//       free(buf);
+//       return 1;
+//   } else {
+//       return 0;
+//   }
+// }
 
 int SDL_WaitEvent(SDL_Event *ev) {
     unsigned buf_size = 32;
