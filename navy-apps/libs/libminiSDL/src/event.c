a3be9b3c7145db3c5ce5628372526ed91030fbfd
tracer-ics2023 <tracer@njuics.org>
2024-07-27 23:20:16 +0800

    >  compile NEMU 11451400 lemon Linux lemonsuqing 6.5.0-44-generic #44~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 18 14:36:16 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:20:16 up 15:51,  1 user,  load average: 0.74, 0.57, 0.48

--- navy-apps/libs/libminiSDL/src/event.c
+++ navy-apps/libs/libminiSDL/src/event.c
@@ -38,18 +38,6 @@ int SDL_PollEvent(SDL_Event *ev) {
         break;
       }
     }
-
-    // Handle special cases for keys like space
-    // if (!flag) {
-    //   printf("buf = %s\n",buf);
-    //   if (strncmp(buf + 3, "SPACE", strlen(buf) - 4) == 0) {
-    //     ev->key.keysym.sym = SDLK_SPACE;  // Use SDLK_SPACE for SDL space key
-    //     flag = 1;
-    //   } else if (strncmp(buf + 3, "ENTER", strlen(buf) - 4) == 0) {
-    //     ev->key.keysym.sym = SDLK_RETURN;  // Use SDLK_RETURN for SDL enter key
-    //     flag = 1;
-    //   }
-    // }
     
     free(buf);
     return 1;
