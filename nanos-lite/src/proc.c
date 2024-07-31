e434324a9c935a6775d38b4be1d5f72696369f1b
tracer-ics2023 <tracer@njuics.org>
2024-07-31 19:35:32 +0800

    >  compile NEMU 11451400 lemon Linux lemonsuqing 6.5.0-44-generic #44~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 18 14:36:16 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:35:32 up 1 day, 58 min,  1 user,  load average: 0.33, 0.19, 0.20

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -23,7 +23,7 @@ void init_proc() {
   switch_boot_pcb();
 
   Log("Initializing processes...");
-  naive_uload(current, "/bin/dummy");
+  naive_uload(current, "/bin/nterm");
   // load program here
 
 }
